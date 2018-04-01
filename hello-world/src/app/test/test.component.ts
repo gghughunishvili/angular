f8168d605e87c5c326bd20c908ae2286738aa2d2
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-01 14:49:28 -0400

    added style binding examples

--- hello-world/src/app/test/test.component.ts
+++ hello-world/src/app/test/test.component.ts
@@ -9,13 +9,20 @@ export class TestComponent implements OnInit {
   public name = "Giorgi";
   public myId = "testID";
   public isDisabled = true;
+  
   public isSpecial = true;
   public hasError = true;
   public messageClasses = {
     "text-success" : !this.hasError,
     "text-error" : this.hasError,
     "text-special" : this.isSpecial,
-  }
+  };
+
+  public highlightColor = "purple";
+  public messageStyles = {
+    fontWeight: "bold",
+    color: "blue"
+  };
 
   public siteUrl = window.location.href;
   
