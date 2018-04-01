2fc0e0dc5d3724691732a0092535a070aa084f24
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-01 12:53:16 -0400

    added test component using via class and via hyphen notation

--- hello-world/src/app/app.module.ts
+++ hello-world/src/app/app.module.ts
@@ -4,12 +4,16 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { TestComponent } from './test/test.component';
+import { TestClassComponent } from './test-class/test-class.component';
+import { TestHyphenComponent } from './test-hyphen/test-hyphen.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    TestComponent,
+    TestClassComponent,
+    TestHyphenComponent
   ],
   imports: [
     BrowserModule
---
+++ hello-world/src/app/test-hyphen/test-hyphen.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: '[app-test-hyphen]',
+  templateUrl: './test-hyphen.component.html',
+  styleUrls: ['./test-hyphen.component.css']
+})
+export class TestHyphenComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
