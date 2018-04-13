d777442ff53e94f2eae757907957f0ab5ebd4b74
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-13 09:15:41 -0400

    added item's removing event with animation

--- ng5/src/app/home/home.component.ts
+++ ng5/src/app/home/home.component.ts
@@ -14,8 +14,14 @@ import { trigger,style,transition,animate,keyframes,query,stagger } from '@angul
             style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
             style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
             style({opacity: 1, transform: 'translateY(0)', offset: 1})
-          ]))
-        ]), {optional: true})
+          ]))]), {optional: true}),
+          
+          query(':leave', stagger('300ms',[
+            animate('.6s ease-in', keyframes([
+              style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+              style({opacity: .5, transform: 'translateY(35px)', offset: .3}),
+              style({opacity: 0, transform: 'translateY(-75%)', offset: 1})
+            ]))]), {optional: true})
       ])
     ])
   ]
@@ -32,10 +38,14 @@ export class HomeComponent implements OnInit {
     this.itemCount = this.goals.length;
   }
 
-  addItem(item) {
+  addItem() {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+  }
+
+  removeItem(i) {
+    this.goals.splice(i, 1);
   }
 
 }
