6f8335f901f63dfea4b4b7d9d4a46bd6b3aa9750
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-14 16:21:15 -0400

    added messageService that was injected in heroService and messageComponent

--- tour-of-heroes/src/app/message.service.ts
+++ tour-of-heroes/src/app/message.service.ts
@@ -2,7 +2,17 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class MessageService {
-
+  
+  messages: string[] = [];
+  
   constructor() { }
+
+  add(message: string) {
+    this.messages.push(message);
+  }
+
+  clear() {
+    this.messages = [];
+  }
 
 }
