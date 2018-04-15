ca083c4b2a7d24f0da0a139aa617d52c441849e7
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-14 23:07:58 -0400

    added routes; added dashboard; simplified getting hero details

--- tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { ActivatedRoute } from '@angular/router';
+import { HeroService } from '../hero.service';
+import { Location } from '@angular/common';
 
 @Component({
   selector: 'app-hero-detail',
@@ -8,11 +11,26 @@ import { Hero } from '../hero';
 })
 export class HeroDetailComponent implements OnInit {
 
-  @Input() hero: Hero;
+  hero: Hero;
 
-  constructor() { }
+  constructor(
+    private _route: ActivatedRoute,
+    private _heroService: HeroService,
+    private _location: Location
+  ) {}
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this._route.snapshot.paramMap.get('id');
+    this._heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this._location.back();
   }
 
 }
