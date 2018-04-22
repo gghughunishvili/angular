a71d6e45cf3b1f36ee09bdeb138b2c41ef1d5ac0
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-22 01:48:15 -0400

    separated product route module

--- GettingStarted/APM-Start/src/app/products/product.module.ts
+++ GettingStarted/APM-Start/src/app/products/product.module.ts
@@ -1,20 +1,15 @@
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ProductGuardService } from './product-guard.service';
 import { ConvertToSpacesPipe } from '../shared/convert-to-spaces.pipe';
 import { ProductService } from './product.service';
 import { SharedModule } from './../shared/shared.module';
+import { ProductRoutingModule } from './product-routing.module';
 
 @NgModule({
   imports: [
-    RouterModule.forRoot([
-      { path: 'products', component: ProductListComponent },
-      { path: 'products/:id', 
-        canActivate: [ ProductGuardService ],
-        component: ProductDetailComponent },
-    ]),
+    ProductRoutingModule,
     SharedModule
   ],
   declarations: [
