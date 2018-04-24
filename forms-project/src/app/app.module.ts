18fa6dbeb70f6bda469f1fdf0f5304916ecd2c46
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-23 23:18:02 -0400

    added home component with basic form input

--- forms-project/src/app/app.module.ts
+++ forms-project/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { HttpModule } from '@angular/http';
 
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
