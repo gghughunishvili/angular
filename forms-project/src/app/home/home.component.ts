78e85796e929b08ceceb1e8e5452765989276cf5
Giorgi Ghughunishvili <giorgi855007@gmail.com>
2018-04-24 23:52:45 -0400

    added two way data binding, by 2 different approach

--- forms-project/src/app/home/home.component.ts
+++ forms-project/src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from '../models/employee.model';
 
 @Component({
   selector: 'app-home',
@@ -8,10 +9,18 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 
   langs = ['English', 'Spanish', 'Georgian', 'Other'];
-
+  employee = new Employee('Emma', 'Watson');
   constructor() { }
 
   ngOnInit() {
+  }
+
+  firstNameToUpperCase(value: string) {
+    if (value.length > 0) {
+      this.employee.firstname = value.charAt(0).toUpperCase() + value.slice(1);
+    } else {
+      this.employee.firstname = value;
+    }
   }
 
 }
